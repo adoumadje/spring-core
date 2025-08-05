b383697016f5c86c4ba62643af92f5b8e3812fbc
adoumadje <adoumadjecaleb@gmail.com>
2025-08-05 09:24:57 +0200

    starter

---
+++ springcoreadvanced/src/main/java/com/adoumadje/springcore/autowiring/Test.java
@@ -0,0 +1,13 @@
+package com.adoumadje.springcore.autowiring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        String config = "com/adoumadje/springcore/autowiring/config.xml";
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(config);
+        Employee employee = applicationContext.getBean("employee", Employee.class);
+        System.out.println(employee);
+    }
+}
